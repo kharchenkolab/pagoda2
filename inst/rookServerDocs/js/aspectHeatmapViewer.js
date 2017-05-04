ae1f401218d6bb66ad276a095aeb21a9ec85ffca
barkasn <Nikolas.barkas@outlook.com>
2017-05-04 18:00:12 -0400

    aspect heatmap download

--- inst/rookServerDocs/js/aspectHeatmapViewer.js
+++ inst/rookServerDocs/js/aspectHeatmapViewer.js
@@ -107,6 +107,19 @@ aspectHeatmapViewer.prototype.generateMenu = function(){
 
   });
 
+  toolbar.add({
+          text: "",
+        type: "button",
+        tooltip: 'Download current view',
+        glyph: 0xf0ed,
+        handler: function(){
+            var canvas = document.getElementById('aspect-heatmap-area');
+            var imageURL = canvas.toDataURL('image/png');
+            imageURL = imageURL.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+            window.open(imageURL);
+        } // handler
+});
+
     // Add plot configuration menu button
     toolbar.add({
     	text: '',
