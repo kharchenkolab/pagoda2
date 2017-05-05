8ce057d1fe7b977c6b41d2953ef4e925bfbdd7a8
barkasn <Nikolas.barkas@outlook.com>
2017-05-05 10:01:35 -0400

    added large canvas dialog for aspect view

--- inst/rookServerDocs/js/aspectHeatmapViewer.js
+++ inst/rookServerDocs/js/aspectHeatmapViewer.js
@@ -114,9 +114,25 @@ aspectHeatmapViewer.prototype.generateMenu = function(){
         glyph: 0xf0ed,
         handler: function(){
             var canvas = document.getElementById('aspect-heatmap-area');
-            var imageURL = canvas.toDataURL('image/png');
-            imageURL = imageURL.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
-            window.open(imageURL);
+
+            const maxSize = 2000;
+            if (canvas.width > maxSize | canvas.height >maxSize){
+                Ext.Msg.show({
+                  title: 'Warning',
+                  msg: 'The current canvas size exceeds ' + maxSize + 'px in at least one dimention.' +
+                   'This may cause problems during exporting. Do you want to continue?',
+                   buttons: Ext.Msg.OKCANCEL,
+                   fn: function(s) {
+                     if (s == 'ok') {
+                          var imageURL = canvas.toDataURL('image/png');
+                          imageURL = imageURL.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+                          window.open(imageURL);
+                     } //if
+                   } //fn
+                }) // Ext.Msg.show
+            } // if
+
+
         } // handler
 });
 
