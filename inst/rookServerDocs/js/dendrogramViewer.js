5bf389573c72861874cc7e719da74fb1f95b0b09
barkasn <Nikolas.barkas@outlook.com>
2017-05-05 10:02:14 -0400

    added large  canvas warning in dendrogram

--- inst/rookServerDocs/js/dendrogramViewer.js
+++ inst/rookServerDocs/js/dendrogramViewer.js
@@ -233,9 +233,25 @@ toolbar.add({
         glyph: 0xf0ed,
         handler: function(){
             var canvas = document.getElementById('dendrogram-area');
-            var imageURL = canvas.toDataURL('image/png');
-            imageURL = imageURL.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
-            window.open(imageURL);
+
+                        const maxSize = 2000;
+            if (canvas.width > maxSize | canvas.height >maxSize){
+                Ext.Msg.show({
+                  title: 'Warning',
+                  msg: 'The current canvas size exceeds ' + maxSize + 'px in at least one dimention.' +
+                   'This may cause problems during exporting. Do you want to continue?',
+                   buttons: Ext.Msg.OKCANCEL,
+                   fn: function(s) {
+                     if (s == 'ok') {
+                          var imageURL = canvas.toDataURL('image/png');
+                          imageURL = imageURL.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+                          window.open(imageURL);
+                     } //if
+                   } //fn
+                }) // Ext.Msg.show
+            } // if
+
+
         } // handler
 });
 
