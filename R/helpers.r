ae02f39f8e37fe58ea8a16f62929b5760743dabd
barkasn <Nikolas.barkas@outlook.com>
2017-05-23 16:12:28 -0400

    Update example and documentation

--- R/helpers.r
+++ R/helpers.r
@@ -53,6 +53,39 @@ p2.generate.human.go <- function(r) {
   go.env
 }
 
+#' @export p2.generate.human.go.web
+p2.generate.human.go.web <- function(myGeneNames) {
+
+  require(org.Hs.eg.db)
+  require(GO.db)
+  require(BiocGenerics)
+  require(AnnotationDbi)
+
+  ids <- unlist(lapply(BiocGenerics::mget(myGeneNames, org.Hs.egALIAS2EG, ifnotfound = NA), function(x) x[1]))
+  rids <- names(ids)
+
+  names(rids) <- ids
+
+  go.env <-
+    AnnotationDbi::eapply(org.Hs.egGO2ALLEGS, function(x)
+      as.character(na.omit(rids[x])))
+  go.env <- go.env[unlist(lapply(go.env, length)) > 5]
+
+  geneSets <- lapply(names(go.env), function(x) {
+    list(
+      properties = list(
+        locked = T,
+        genesetname = x,
+        shortdescription = GOTERM[[x]]@Term
+      ),
+      genes = c(go.env[[x]])
+    )
+  })
+  names(geneSets) <- names(go.env)
+
+
+  geneSets
+}
 
 
 #' @title Generate a metadata structure for a p2 web object from a named factor
