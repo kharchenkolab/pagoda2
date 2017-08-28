32bbc244071268b0d4a755e3a4b6aa27975127ef
barkasn <Nikolas.barkas@outlook.com>
2017-08-28 15:52:23 -0400

    New faster de testing complete

--- inst/rookServerDocs/js/statisticsWorker.js
+++ inst/rookServerDocs/js/statisticsWorker.js
@@ -5,7 +5,7 @@ self.geneName = null;
 self.selAidx = null;
 self.selBidx = null;
 self.method = null;
-self.chunks = 50;
+self.chunks = 20;
 self.minchunksize = 500;
 
 // Main event listener for the worker thread
@@ -76,7 +76,7 @@ function handleInitiateCommand(e) {
 function handleProcessCommand(e) {
   var callParams = e.data;
   if(self.method === "wilcoxon"){
-    runWilcoxonOnGroup(callParams.params, callParams.data);
+    runMannWhitneyIteration(callParams.params, callParams.data);
   } else {
     // TODO: Handle error
     console.log('Unknown method')
@@ -109,27 +109,39 @@ function handleProcessCommand(e) {
  * @param params A compound object containing data, and information passed to this worker from the event listener
  * @param geneData A sparse matrix containing the gene names being read in and the expression values
  */
-function runWilcoxonOnGroup(params, geneData){
+function runMannWhitneyIteration(params, geneData){
 
       for(var geneindex = 0; geneindex < geneData.array[0].length; geneindex++){
 
         var allValues = [];
 
+        var nNonZeroA = 0;
+        var nNonZeroB = 0;
+
+        // Skip sparse genes
+        var sparseFractionCutoff = 0.10;
+
         //retrieve expression data by indexes for selection A
         for(var cell = 0; cell < self.selAidx.length; cell++){
+          var eVal = geneData.array[self.selAidx[cell]][geneindex];
+          if (eVal != 0) nNonZeroA++;
           allValues.push({
             selection: 1,
-            exprVal: geneData.array[self.selAidx[cell]][geneindex]
+            exprVal: eVal
           });
         }
+        if(nNonZeroA < self.selAidx.length * sparseFractionCutoff) continue;
 
         //retrieve expression data by indexes for selection B
         for(var cell = 0; cell < self.selBidx.length; cell++){
+          var eVal = geneData.array[self.selBidx[cell]][geneindex];
+          if (eVal != 0) nNonZeroB++;
           allValues.push({
             selection: 2,
-            exprVal: geneData.array[self.selBidx[cell]][geneindex]
+            exprVal: eVal
           });
         }
+        if(nNonZeroB < self.selBidx.length * sparseFractionCutoff) continue;
 
         // Sort and calculate total ranks
         allValues.sort(function(x,y){return x.exprVal - y.exprVal});
@@ -138,29 +150,30 @@ function runWilcoxonOnGroup(params, geneData){
         var lastVal = allValues[0].exprVal;
         var lastValStartIndex = 0;
         var inTie = false;
-        
+
         // Calculate element ranks taking into account ties
         for (var i = 0; i < allValues.length; i++) {
           // Set the rank to the position in the array (1-indexed)
           allValues[i].rank = i + 1;
-          
+
           if (allValues[i].exprVal === lastVal) {
             // In a tie
             if (!inTie) {
               // Just entered the tie
-              lastValStartIndex = i - 1;
+              lastValStartIndex = i;
+              lastVal = allValues[i].exprVal;
               inTie = true;
-            } 
+            }
             // else we were already in a tie and we don't need to do anything
           } else {
             // Not in a tie
             if (inTie) {
               // Just exited the previous tie
-              var commonRank = (lastValStartIndex + 1) + (i + 1) / (i - lastValStartIndex);
+              var commonRank = (lastValStartIndex + 1) + (i + 1) / (i - lastValStartIndex + 1);
               for (var j = lastValStartIndex; j < i; j++) {
                 allValues[j].rank = commonRank;
               } // for
-              
+
               inTie = false;
             } else { // Not in tie
               lastVal = allValues[i].exprVal;
@@ -169,30 +182,26 @@ function runWilcoxonOnGroup(params, geneData){
           } // if == lastVa else
         } // for i
 
-        // Calculate rank sums
+        // Calculate rank sum for A
         var totalRankA = 0;
-        var totalRankB = 0;
         for (var i = 0; i < allValues.length; i++) {
-          if (allValues[i].selection == 1){
+          if (allValues[i].selection === 1){
             totalRankA += (i+1);
-          } else {
-            totalRankB += (i+1);
           }
         }
 
         // Calculate U values
         var lengthA = self.selAidx.length;
         var lengthB = self.selBidx.length;
-        var u1 = totalRankA - (lengthA * (lengthA +1)) /2
-        var u2 = totalRankB - (lengthB * (lengthB +1)) /2
+        var lenghtAxlengthB = lengthA * lengthB; //u1u2
+        var u1 = totalRankA - (lengthA * (lengthA +1)) /2;
+        var u2 = lenghtAxlengthB - u1;
         var U = Math.min(u1, u2);
 
         // Perform Normal approximation with tie correction
-        var lenghtAxlengthB = lengthA * lengthB;
-        var muU = lenghtAxlengthB /2;
-        var sigmaU = Math.sqrt(lenghtAxlengthB * (lengthA + lengthB + 1) / 12)
-        
-        // Calculate rank abundance 
+        var muU = lenghtAxlengthB / 2;
+
+        // Calculate rank abundance and Loop over rank counts and calculate K
         var rankCounts = {};
         for (var i = 0; i < allValues.length; i++) {
           var rank = allValues[i].rank;
@@ -202,8 +211,6 @@ function runWilcoxonOnGroup(params, geneData){
             rankCounts[rank] = rankCounts[rank] + 1;
           }
         }
-        
-        // Loop over rank counts and calculate K
         var K = 0;
         var n = lengthA + lengthB;
         var ranks = Object.keys(rankCounts);
@@ -211,26 +218,43 @@ function runWilcoxonOnGroup(params, geneData){
           var ti = rankCounts[ranks[i]];
           K = K + ( (Math.pow(ti,3) - ti) / (n * (n-1) ) );
         }
-        
-        var sigmaUcorr = Math.sqrt( lenghtAxlengthB / 12 * ( lengthA + lengthB + 1 ) - K);
-        
-        //var z = (U - muU) / sigmaU;
-        var z = (U - muU) / sigmaUcorr;
-        
-        var zAbs = Math.abs(z);
 
-        // accepts p < .05
-        if(zAbs >= 3.0){
+        // Calculate corrected sigma
+        var sigmaUcorr = Math.sqrt( lenghtAxlengthB / 12 * ( n + 1 ) - K);
+
+        // Calculate corrected Z score
+        var z = (U - muU) / sigmaUcorr;
+
+        const zcutoff = 4.0;
+        var zAbs = Math.abs(z);
+        if(zAbs >= zcutoff){
+          // TODO: Calculate fe and M
+
+          var sumA = 0;
+          var nA = self.selAidx.length;
+          for(var cell = 0; cell < nA; cell++){
+            sumA += geneData.array[self.selAidx[cell]][geneindex];
+          }
+          var meanA = sumA / nA;
+
+          var sumB = 0;
+          var nB = self.selBidx.length;
+          for(var cell = 0; cell < nB; cell++){
+            sumB += geneData.array[self.selBidx[cell]][geneindex];
+          }
+          var meanB = sumB /nB;
+
           self.collectedResults.push(
             {
               Z: z,
               absZ: zAbs,
               name: geneData.colnames[geneindex],
-              fe: 0,
-              M: 0,
+              fe: Math.log(meanA/meanB)/ Math.log(10),
+              M: (sumA+sumB)/(nA+nB),
               highest: false
             }
           )
-        } // if(z >= 3.0)
+        } // zAbs >= zcutoff
+
       }
 }
