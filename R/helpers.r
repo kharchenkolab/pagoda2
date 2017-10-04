f54f8339be49ff4068fd66feef26ece5962ab0e5
barkasn <Nikolas.barkas@outlook.com>
2017-10-04 11:38:49 -0400

    Cleanups

--- R/helpers.r
+++ R/helpers.r
@@ -6,7 +6,165 @@
 NULL
 
 
+# translate multilevel segmentation into a dendrogram, with the lowest level of the dendrogram listing the cells
+multi2dend <- function(cl,counts,deep=F) {
+  if(deep) {
+    clf <- as.integer(cl$memberships[1,]); # take the lowest level
+  } else {
+    clf <- as.integer(membership(cl));
+  }
+  names(clf) <- names(membership(cl))
+  clf.size <- unlist(tapply(clf,factor(clf,levels=seq(1,max(clf))),length))
+  rowFac <- rep(NA,nrow(counts));
+  rowFac[match(names(clf),rownames(counts))] <- clf;
+  lvec <- colSumByFac(counts,rowFac)[-1,,drop=F];
+  lvec.dist <- jsDist(t(lvec/pmax(1,Matrix::rowSums(lvec))));
+  d <- as.dendrogram(hclust(as.dist(lvec.dist),method='ward.D'))
+  # add cell info to the laves
+  addinfo <- function(l,env) {
+    v <- as.integer(mget("index",envir=env,ifnotfound=0)[[1]])+1;
+    attr(l,'nodeId') <- v
+    assign("index",v,envir=env)
+    attr(l,'nCells') <- sum(clf.size[as.integer(unlist(l))]);
+    if(is.leaf(l)) {
+      attr(l,'cells') <- names(clf)[clf==attr(l,'label')];
+    }
+    attr(l,'root') <- FALSE;
+    return(l);
+  }
+  d <- dendrapply(d,addinfo,env=environment())
+  attr(d,'root') <- TRUE;
+  d
+}
 
+
+# quick utility to check if given character vector is colors
+# thanks, Josh O'Brien: http://stackoverflow.com/questions/13289009/check-if-character-string-is-a-valid-color-representation
+areColors <- function(x) {
+  is.character(x) & sapply(x, function(X) {tryCatch(is.matrix(col2rgb(X)), error = function(e) FALSE)})
+}
+
+papply <- function(...,n.cores=detectCores(), mc.preschedule=FALSE) {
+  if(n.cores>1) {
+    # bplapply implementation
+    if(is.element("parallel", installed.packages()[,1])) {
+      mclapply(...,mc.cores=n.cores,mc.preschedule=mc.preschedule)
+    } else {
+      # last resort
+      bplapply(... , BPPARAM = MulticoreParam(workers = n.cores))
+    }
+  } else { # fall back on lapply
+    lapply(...);
+  }
+}
+jw.disR <- function(x,y) {
+  x <- x+1/length(x)/1e3;
+  y <- y+1/length(y)/1e3;
+  a <- x*log(x)  + y*log(y) - (x+y)*log((x+y)/2);
+  sqrt(sum(a)/2)
+}
+
+
+# note transpose is meant to speed up calculations when neither scaling nor centering is required
+fast.pca <- function(m,nPcs=2,tol=1e-10,scale=F,center=F,transpose=F) {
+  require(irlba)
+  if(transpose) {
+    if(center) { m <- m-Matrix::rowMeans(m)}; if(scale) { m <- m/sqrt(Matrix::rowSums(m*m)); }
+    a <- irlba(tcrossprod(m)/(ncol(m)-1), nu=0, nv=nPcs,tol=tol);
+    a$l <- t(t(a$v) %*% m)
+  } else {
+    if(scale||center) { m <- scale(m,scale=scale,center=center) }
+    #a <- irlba((crossprod(m) - nrow(m) * tcrossprod(Matrix::colMeans(m)))/(nrow(m)-1), nu=0, nv=nPcs,tol=tol);
+    a <- irlba(crossprod(m)/(nrow(m)-1), nu=0, nv=nPcs,tol=tol);
+    a$l <- m %*% a$v
+  }
+  a
+}
+
+
+# a utility function to translate factor into colors
+fac2col <- function(x,s=1,v=1,shuffle=FALSE,min.group.size=1,return.details=F,unclassified.cell.color='gray50',level.colors=NULL) {
+  x <- as.factor(x);
+  if(min.group.size>1) {
+    x <- factor(x,exclude=levels(x)[unlist(tapply(rep(1,length(x)),x,length))<min.group.size])
+    x <- droplevels(x)
+  }
+  if(is.null(level.colors)) {
+    col <- rainbow(length(levels(x)),s=s,v=v);
+  } else {
+    col <- level.colors[1:length(levels(x))];
+  }
+  names(col) <- levels(x);
+
+  if(shuffle) col <- sample(col);
+
+  y <- col[as.integer(x)]; names(y) <- names(x);
+  y[is.na(y)] <- unclassified.cell.color;
+  if(return.details) {
+    return(list(colors=y,palette=col))
+  } else {
+    return(y);
+  }
+}
+
+val2col <- function(x,gradientPalette=NULL,zlim=NULL,gradient.range.quantile=0.95) {
+  if(all(sign(x)>=0)) {
+    if(is.null(gradientPalette)) {
+      gradientPalette <- colorRampPalette(c('gray90','red'), space = "Lab")(1024)
+    }
+    if(is.null(zlim)) {
+      zlim <- as.numeric(quantile(na.omit(x),p=c(1-gradient.range.quantile,gradient.range.quantile)))
+      if(diff(zlim)==0) {
+        zlim <- as.numeric(range(na.omit(x)))
+      }
+    }
+    x[x<zlim[1]] <- zlim[1]; x[x>zlim[2]] <- zlim[2];
+    x <- (x-zlim[1])/(zlim[2]-zlim[1])
+
+  } else {
+    if(is.null(gradientPalette)) {
+      gradientPalette <- colorRampPalette(c("blue", "grey90", "red"), space = "Lab")(1024)
+    }
+    if(is.null(zlim)) {
+      zlim <- c(-1,1)*as.numeric(quantile(na.omit(abs(x)),p=gradient.range.quantile))
+      if(diff(zlim)==0) {
+        zlim <- c(-1,1)*as.numeric(na.omit(max(abs(x))))
+      }
+    }
+    x[x<zlim[1]] <- zlim[1]; x[x>zlim[2]] <- zlim[2];
+    x <- (x-zlim[1])/(zlim[2]-zlim[1])
+
+  }
+
+  gradientPalette[x*(length(gradientPalette)-1)+1]
+}
+
+
+
+# translate cell cluster dendrogram to an array, one row per node with 1/0 cluster membership
+cldend2array <- function(d,cells=NULL) {
+  if(is.null(cells)) { # figure out the total order of cells
+    cells <- unlist(dendrapply(d,attr,'cells'))
+  }
+  getcellbin <- function(l) {
+    if(is.leaf(l)) {
+      vi <- match(attr(l,'cells'),cells)
+      ra <- sparseMatrix(i=vi,p=c(0,length(vi)),x=rep(1,length(vi)),dims=c(length(cells),1),dimnames=list(NULL,attr(l,'nodeId')))
+      return(ra);
+    } else { # return rbind of the children arrays, plus your own
+      ra <- do.call(cbind,lapply(l,getcellbin))
+      ur <- unique(ra@i);
+      ra <- cbind(sparseMatrix(ur+1,x=rep(1,length(ur)),p=c(0,length(ur)),dims=c(length(cells),1),dimnames=list(NULL,attr(l,'nodeId'))),ra);
+      return(ra)
+    }
+  }
+  a <- getcellbin(d)
+  rownames(a) <- cells;
+  return(t(a));
+}
+
+
+sn <- function(x) { names(x) <- x; return(x); }
 
 #' @export p2.generate.human.go
 p2.generate.human.go <- function(r) {
