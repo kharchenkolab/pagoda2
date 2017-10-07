71e279512596cd85abee2fe803c012d1d5c624bd
barkasn <nikolas.barkas@outlook.com>
2017-10-07 19:27:24 -0400

    fixed p2.generate.go.web() and added n.cores param

--- R/pagodaWebHelpers.R
+++ R/pagodaWebHelpers.R
@@ -10,18 +10,18 @@ NULL
 #' @description generates a humna go annotation for the pagoda2 web object
 #' @param gene.names a character vector of genes to include
 #' @export p2.generate.human.go.web
-p2.generate.human.go.web <- function(gene.names) {
+p2.generate.human.go.web <- function(gene.names, n.cores = 1) {
   require(org.Hs.eg.db)
-  p2.generate.go.web(gene.names = gene.names,  egALIAS2EG = org.Hs.egALIAS2EG, egGO2ALLEGS = org.Hs.egGO2ALLEGS);
+  p2.generate.go.web(gene.names = gene.names,  egALIAS2EG = org.Hs.egALIAS2EG, egGO2ALLEGS = org.Hs.egGO2ALLEGS, n.cores = n.cores);
 }
 
 #' Generates mouse go annotation for the web object
 #' @description generates a humna go annotation for the pagoda2 web object
 #' @param gene.names a character vector of genes to include
 #' @export p2.generate.mouse.go.web
-p2.generate.mouse.go.web <- function(gene.names) {
+p2.generate.mouse.go.web <- function(gene.names, n.cores = 1) {
   require(org.Mm.eg.db)
-  p2.generate.go.web(gene.names = gene.names,  egALIAS2EG = org.Mm.egALIAS2EG, egGO2ALLEGS = org.Mm.egGO2ALLEGS);
+  p2.generate.go.web(gene.names = gene.names,  egALIAS2EG = org.Mm.egALIAS2EG, egGO2ALLEGS = org.Mm.egGO2ALLEGS, n.cores = n.cores);
 }
 
 
@@ -29,51 +29,42 @@ p2.generate.mouse.go.web <- function(gene.names) {
 #' @description generates a humna go annotation for the pagoda2 web object
 #' @param gene.names a character vector of genes to include
 #' @export p2.generate.human.go.web
-p2.generate.go.web <- function(gene.names, egALIAS2EG = NULL, egGO2ALLEGS = NULL) {
-  require(GO.db)
-  require(BiocGenerics)
-  require(AnnotationDbi)
-  require(parallel)
-
-  if (is.null(egALIAS2EG)) {
-    stop('egALIAS2EG cannot be null, it has to be an object like org.Hs.egALIAS2EG');
-  }
-
-  if (is.null(org.Hs.egGO2ALLEGS)) {
-    stop('org.Hs.egGO2ALLEGS cannot be null it has to be an object like org.Hs.egGO2ALLEGS');
-  }
-
-  if (!is.character(gene.names)) {
-    stop("gene.names needs to be a character vector of gene names");
-  }
-
-  ids <- unlist(mclapply(BiocGenerics::mget(gene.names, egALIAS2EG, ifnotfound = NA), function(x) x[1]))
-
-  # Swap names and ids
-  rids <- names(ids)
-  names(rids) <- ids
-
-  # Get go environment
-  go.env <- AnnotationDbi::eapply(egGO2ALLEGS, function(x) as.character(na.omit(rids[x])))
-
-  # Filter for go terms with at least 5 genes
-  go.env <- go.env[unlist(lapply(go.env, length)) > 5]
-
-  # TODO make this parallel
-  geneSets <- lapply(names(go.env), function(x) {
-    list(
-      properties = list(
-        locked = T,
-        genesetname = x,
-        shortdescription = GO.db::GOTERM[[x]]@Term
-      ),
-      genes = c(go.env[[x]])
-    )
-  })
-
-  # Name the genesets
-  names(geneSets) <- names(go.env)
-
-  # return geneSets
-  invisible(geneSets)
-}
+p2.generate.go.web  <-                                                                                                                                                                                   
+  function (gene.names, egALIAS2EG = NULL, egGO2ALLEGS = NULL, n.cores = 1)                                                                                                                            
+  {                                                                                                                                                                                                        
+    require(GO.db)                                                                                                                                                                                       
+    require(BiocGenerics)                                                                                                                                                                                
+    require(AnnotationDbi)                                                                                                                                                                               
+    require(parallel)                                                                                                                                                                                    
+    if (is.null(egALIAS2EG)) {                                                                                                                                                                           
+      stop("egALIAS2EG cannot be null, it has to be an object like org.Hs.egALIAS2EG")                                                                                                                 
+    }                                                                                                                                                                                                    
+    if (is.null(org.Hs.egGO2ALLEGS)) {                                                                                                                                                                   
+      stop("org.Hs.egGO2ALLEGS cannot be null it has to be an object like org.Hs.egGO2ALLEGS")                                                                                                         
+    }                                                                                                                                                                                                    
+    if (!is.character(gene.names)) {                                                                                                                                                                     
+      stop("gene.names needs to be a character vector of gene names")                                                                                                                                  
+    }                                                                                                                                                                                                    
+    
+    ids <- unlist(mclapply(mget(myGeneNames, org.Hs.egALIAS2EG, ifnotfound = NA), function(x) x[1], mc.cores = n.cores))                                                                                 
+    rids <- names(ids)                                                                                                                                                                                   
+    names(rids) <- ids                                                                                                                                                                                   
+    go.env <- AnnotationDbi::eapply(org.Hs.egGO2ALLEGS, function(x) as.character(na.omit(rids[x])))                                                                                                      
+    go.env <- go.env[unlist(lapply(go.env, length)) > 5]                                                                                                                                                 
+    
+    ## Put the GO Term annotation generated in a format suitable for the web interface                                                                                                                   
+    nms <- names(go.env);                                                                                                                                                                                
+    names(nms) <- nms;                                                                                                                                                                                   
+    geneSets <- lapply(names(go.env), function(x) {                                                                                                                                                      
+      list(                                                                                                                                                                                            
+        properties = list(                                                                                                                                                                           
+          locked = T,                                                                                                                                                                              
+          genesetname = x,                                                                                                                                                                         
+          shortdescription = GOTERM[[x]]@Term                                                                                                                                                      
+        ),                                                                                                                                                                                           
+        genes = c(go.env[[x]])                                                                                                                                                                       
+      )                                                                                                                                                                                                
+    })                                                                                                                                                                                                   
+    
+    invisible(geneSets)                                                                                                                                                                                         
+  }   
