ba34b5e67b6a7505488f2bca2dd86784a75803e2
evanbiederstedt <evan.biederstedt@gmail.com>
2025-08-17 14:49:58 -0400

    update docs

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -2,11 +2,15 @@
 // Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 #include <RcppArmadillo.h>
-#define NDEBUG 1
 #include <RcppEigen.h>
 #include <Rcpp.h>
 
 using namespace Rcpp;
+
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
 
 // WriteListToBinary
 void WriteListToBinary(List expL, std::string outfile, bool verbose);
